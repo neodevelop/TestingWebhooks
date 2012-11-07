1685993a2a29c4bbffd8df3d8fbe4ac4c2e7ae1b
neodevelop <neodevelop@gmail.com>
2012-11-06 23:55:30 -0600

    adding println

--- grails-app/controllers/org/recursivo/NotificationController.groovy
+++ grails-app/controllers/org/recursivo/NotificationController.groovy
@@ -3,10 +3,12 @@ package org.recursivo
 class NotificationController {
 
   def index() {
+    println params
     log.debug params
   }
 
   def payload(){
+    println params
     log.debug params
   }  
 
